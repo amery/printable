204da7e4516b2877beac7dad267efb21c909e399
Alejandro Mery <amery@geeks.cl>
2010-06-10 20:45:06 +0200

    url_encode: added encoder for query_string chunks

---
+++ url_encode.c
@@ -0,0 +1,47 @@
+#include "url_encode.h"
+
+#define hex "0123456789ABCDEF"
+
+size_t url_encode(uint8_t c, char *out)
+{
+	if (c>0x1f && c<0x7f) { /* ASCII printable characters */
+		switch(c) {
+		case '$': /* reserved */
+		case '&':
+		case '+':
+		case ',':
+		case '/':
+		case ':':
+		case ';':
+		case '=':
+		case '?':
+		case '@':
+		case ' ': /* unsafe */
+		case '<':
+		case '>':
+		case '#':
+		case '%':
+		case '{':
+		case '}':
+		case '|':
+		case '\\':
+		case '^':
+		case '~':
+		case '[':
+		case ']':
+		case '`':
+			break; /* encode */
+		default:
+			/* raw */
+			if (out) { out[0] = c; }
+			return 1;
+		}
+	}
+
+	if (out) {
+		out[0] = '%';
+		out[1] = hex[(c & (0x0f << 4)) >> 4];
+		out[2] = hex[c & 0x0f];
+	}
+	return 3;
+}
---
+++ url_encode.h
@@ -0,0 +1,9 @@
+#ifndef _URL_ENCODE_H
+#define _URL_ENCODE_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+size_t url_encode(uint8_t c, char *out);
+
+#endif
